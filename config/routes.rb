d7a4a7d934efb8c8b8a146943951b6b197cf54b8
kirykr <kirymeas@gmail.com>
2016-10-19 11:45:39 +0700

    The initialization of the project

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  get 'welcome/loggedin'
+
+  devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations',  }
+  
   post '/signup' => 'splash#signup', as: :splash_signup
   get '/splash' => 'splash#index'
   root 'splash#index'
